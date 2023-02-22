b4f421e9493cd689ba4c5b94f9115502fc2e299b
Thiago Elias <contatothiagoelias22@gmail.com>
2023-02-22 00:48:06 -0300

    Adicionada as funções para os valores dos atributors

--- js/script.js
+++ js/script.js
@@ -116,6 +116,119 @@ function atrConst() {
   } 
 }
 
+function atrInt() {
   let int = Number(document.getElementById('INT').value)
+  let val = document.getElementById('valor-int')
+
+  if (int === 10 || int === 11) {
+    val.innerHTML = 0
+  } else if (int === 9 || int === 8) {
+    val.innerHTML = -1
+  } else if (int === 7 || int === 6) {
+    val.innerHTML = -2
+  } else if (int === 5 || int === 4) {
+    val.innerHTML = -3
+  } else if (int === 3 || int === 2) {
+    val.innerHTML = -4
+  } else if (int === 1 || int === 0) {
+    val.innerHTML = -5
+  } else if (int === 12 || int === 13) {
+    val.innerHTML = 1
+  } else if (int === 14 || int === 15) {
+    val.innerHTML = 2
+  } else if (int === 16 || int === 17) {
+    val.innerHTML = 3
+  } else if (int === 18 || int === 19) {
+    val.innerHTML = 4
+  } else if (int === 20 || int === 21) {
+    val.innerHTML = 5
+  } else if (int === 22 || int === 23) {
+    val.innerHTML = 6
+  } else if (int === 24 || int === 25) {
+    val.innerHTML = 7
+  } else if (int === 26 || int === 27) {
+    val.innerHTML = 8
+  } else if (int === 28 || int === 29) {
+    val.innerHTML = 9
+  } else if (int === 30 || int === 31) {
+    val.innerHTML = 10
+  } 
+}
+
+function atrSab() {
   let sab = Number(document.getElementById('SAB').value)
+  let val = document.getElementById('valor-sab')
+
+  if (sab === 10 || sab === 11) {
+    val.innerHTML = 0
+  } else if (sab === 9 || sab === 8) {
+    val.innerHTML = -1
+  } else if (sab === 7 || sab === 6) {
+    val.innerHTML = -2
+  } else if (sab === 5 || sab === 4) {
+    val.innerHTML = -3
+  } else if (sab === 3 || sab === 2) {
+    val.innerHTML = -4
+  } else if (sab === 1 || sab === 0) {
+    val.innerHTML = -5
+  } else if (sab === 12 || sab === 13) {
+    val.innerHTML = 1
+  } else if (sab === 14 || sab === 15) {
+    val.innerHTML = 2
+  } else if (sab === 16 || sab === 17) {
+    val.innerHTML = 3
+  } else if (sab === 18 || sab === 19) {
+    val.innerHTML = 4
+  } else if (sab === 20 || sab === 21) {
+    val.innerHTML = 5
+  } else if (sab === 22 || sab === 23) {
+    val.innerHTML = 6
+  } else if (sab === 24 || sab === 25) {
+    val.innerHTML = 7
+  } else if (sab === 26 || sab === 27) {
+    val.innerHTML = 8
+  } else if (sab === 28 || sab === 29) {
+    val.innerHTML = 9
+  } else if (sab === 30 || sab === 31) {
+    val.innerHTML = 10
+  }
+}
+
+function atrCar() {
   let car = Number(document.getElementById('CAR').value)
+  let val = document.getElementById('valor-car')
+
+  if (car === 10 || car === 11) {
+    val.innerHTML = 0
+  } else if (car === 9 || car === 8) {
+    val.innerHTML = -1
+  } else if (car === 7 || car === 6) {
+    val.innerHTML = -2
+  } else if (car === 5 || car === 4) {
+    val.innerHTML = -3
+  } else if (car === 3 || car === 2) {
+    val.innerHTML = -4
+  } else if (car === 1 || car === 0) {
+    val.innerHTML = -5
+  } else if (car === 12 || car === 13) {
+    val.innerHTML = 1
+  } else if (car === 14 || car === 15) {
+    val.innerHTML = 2
+  } else if (car === 16 || car === 17) {
+    val.innerHTML = 3
+  } else if (car === 18 || car === 19) {
+    val.innerHTML = 4
+  } else if (car === 20 || car === 21) {
+    val.innerHTML = 5
+  } else if (car === 22 || car === 23) {
+    val.innerHTML = 6
+  } else if (car === 24 || car === 25) {
+    val.innerHTML = 7
+  } else if (car === 26 || car === 27) {
+    val.innerHTML = 8
+  } else if (car === 28 || car === 29) {
+    val.innerHTML = 9
+  } else if (car === 30 || car === 31) {
+    val.innerHTML = 10
+  }
+}
