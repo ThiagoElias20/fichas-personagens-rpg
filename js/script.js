15baa2a6eb778b73b84c2fb29624efd1dc7d3cef
Thiago Elias <contatothiagoelias22@gmail.com>
2023-02-22 00:39:16 -0300

    Js dos atributos

---
+++ js/script.js
@@ -0,0 +1,121 @@
+
+function atrForc() {
+  let forca = Number(document.getElementById('FOR').value)
+  let val = document.getElementById('valor-for')
+
+  if (forca === 10 || forca === 11) {
+    val.innerHTML = 0
+  } else if (forca === 9 || forca === 8) {
+    val.innerHTML = -1
+  } else if (forca === 7 || forca === 6) {
+    val.innerHTML = -2
+  } else if (forca === 5 || forca === 4) {
+    val.innerHTML = -3
+  } else if (forca === 3 || forca === 2) {
+    val.innerHTML = -4
+  } else if (forca === 1 || forca === 0) {
+    val.innerHTML = -5
+  } else if (forca === 12 || forca === 13) {
+    val.innerHTML = 1
+  } else if (forca === 14 || forca === 15) {
+    val.innerHTML = 2
+  } else if (forca === 16 || forca === 17) {
+    val.innerHTML = 3
+  } else if (forca === 18 || forca === 19) {
+    val.innerHTML = 4
+  } else if (forca === 20 || forca === 21) {
+    val.innerHTML = 5
+  } else if (forca === 22 || forca === 23) {
+    val.innerHTML = 6
+  } else if (forca === 24 || forca === 25) {
+    val.innerHTML = 7
+  } else if (forca === 26 || forca === 27) {
+    val.innerHTML = 8
+  } else if (forca === 28 || forca === 29) {
+    val.innerHTML = 9
+  } else if (forca === 30 || forca === 31) {
+    val.innerHTML = 10
+  } 
+}
+
+function atrDestr() {
+  let destr = Number(document.getElementById('DES').value)
+  let val = document.getElementById('valor-dest')
+
+  if (destr === 10 || destr === 11) {
+    val.innerHTML = 0
+  } else if (destr === 9 || destr === 8) {
+    val.innerHTML = -1
+  } else if (destr === 7 || destr === 6) {
+    val.innerHTML = -2
+  } else if (destr === 5 || destr === 4) {
+    val.innerHTML = -3
+  } else if (destr === 3 || destr === 2) {
+    val.innerHTML = -4
+  } else if (destr === 1 || destr === 0) {
+    val.innerHTML = -5
+  } else if (destr === 12 || destr === 13) {
+    val.innerHTML = 1
+  } else if (destr === 14 || destr === 15) {
+    val.innerHTML = 2
+  } else if (destr === 16 || destr === 17) {
+    val.innerHTML = 3
+  } else if (destr === 18 || destr === 19) {
+    val.innerHTML = 4
+  } else if (destr === 20 || destr === 21) {
+    val.innerHTML = 5
+  } else if (destr === 22 || destr === 23) {
+    val.innerHTML = 6
+  } else if (destr === 24 || destr === 25) {
+    val.innerHTML = 7
+  } else if (destr === 26 || destr === 27) {
+    val.innerHTML = 8
+  } else if (destr === 28 || destr === 29) {
+    val.innerHTML = 9
+  } else if (destr === 30 || destr === 31) {
+    val.innerHTML = 10
+  } 
+}
+
+function atrConst() {
+  let cons = Number(document.getElementById('CON').value)
+  let val = document.getElementById('valor-const')
+
+  if (cons === 10 || cons === 11) {
+    val.innerHTML = 0
+  } else if (cons === 9 || cons === 8) {
+    val.innerHTML = -1
+  } else if (cons === 7 || cons === 6) {
+    val.innerHTML = -2
+  } else if (cons === 5 || cons === 4) {
+    val.innerHTML = -3
+  } else if (cons === 3 || cons === 2) {
+    val.innerHTML = -4
+  } else if (cons === 1 || cons === 0) {
+    val.innerHTML = -5
+  } else if (cons === 12 || cons === 13) {
+    val.innerHTML = 1
+  } else if (cons === 14 || cons === 15) {
+    val.innerHTML = 2
+  } else if (cons === 16 || cons === 17) {
+    val.innerHTML = 3
+  } else if (cons === 18 || cons === 19) {
+    val.innerHTML = 4
+  } else if (cons === 20 || cons === 21) {
+    val.innerHTML = 5
+  } else if (cons === 22 || cons === 23) {
+    val.innerHTML = 6
+  } else if (cons === 24 || cons === 25) {
+    val.innerHTML = 7
+  } else if (cons === 26 || cons === 27) {
+    val.innerHTML = 8
+  } else if (cons === 28 || cons === 29) {
+    val.innerHTML = 9
+  } else if (cons === 30 || cons === 31) {
+    val.innerHTML = 10
+  } 
+}
+
+  let int = Number(document.getElementById('INT').value)
+  let sab = Number(document.getElementById('SAB').value)
+  let car = Number(document.getElementById('CAR').value)
